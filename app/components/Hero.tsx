c5f1a6106ddbcea885631f103d1853ba9f27b6d4
unknown <mohammadmarufgazi@gmail.com>
2023-05-23 21:24:04 +0600

    hero section complete

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className=" flex  h-screen w-full bg-gradient-to-b from-black to-gray-800">
       <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className=" text-white flex flex-col justify-center h-full space-y-5">
-          <h2 className=" text-4xl md:text-7xl font-bold">
+          <h2 className=" text-4xl md:text-7xl font-bold mt-16 md:mt-0">
             I'm a Full Stack Developer
           </h2>
           <p className=" text-gray-400 max-w-md">
@@ -16,7 +16,7 @@ const Hero = () => {
             : React, TailwindCss, Redux, Next JS & Node JS{" "}
           </p>
           <div>
-            <button className=" group px-6 py-3 my-2 w-fit flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+            <button className=" group px-6 py-3 my-2 md:w-fit flex justify-center w-full items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
               Portfolio{" "}
               <span className=" group-hover:rotate-90 duration-300">
                 <RiArrowRightSLine size={25} className=" ml-1" />
@@ -28,7 +28,7 @@ const Hero = () => {
           <Image
             src={heroImage}
             alt="hero image"
-            className=" rounded-2xl mx-auto w-2/3 md:w-full"
+            className=" rounded-2xl mx-auto  mt-11 md:mt-0  w-full md:w-[500px]"
           />
         </div>
       </div>
--- app/components/SocialLink.tsx
+++ app/components/SocialLink.tsx
@@ -1,5 +1,43 @@
+import Link from "next/link";
+import { BsFillPersonFill } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
 const SocialLink = () => {
-  return <div>SocialLink</div>;
+  return (
+    <div className=" hidden md:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        <Link
+          target=" _blank"
+          href="https://www.linkedin.com/in/mohammad-maruf-a77b9621a/"
+          className=" flex justify-between items-center w-48 h-14 ml-[-120px] hover:rounded-md duration-300 hover:ml-[-10px] px-6  bg-slate-500 text-white"
+        >
+          Linkedin <FaLinkedin size={30} />
+        </Link>
+        <Link
+          href="https://github.com/Maruf200008"
+          target=" _blank"
+          className=" flex justify-between items-center w-48 h-14 ml-[-120px] hover:rounded-md duration-300 hover:ml-[-10px] px-6  bg-slate-500 text-white"
+        >
+          Github <FaGithub size={30} />
+        </Link>
+        <Link
+          target=" _blank"
+          href="mailto:mohammadmarufgazi@gmail.com"
+          className=" flex justify-between items-center w-48 h-14 ml-[-120px] hover:rounded-md duration-300 hover:ml-[-10px] px-6  bg-slate-500 text-white"
+        >
+          Mail <HiOutlineMail size={30} />
+        </Link>
+        <Link
+          href="#"
+          className=" flex justify-between items-center w-48 h-14 ml-[-120px] hover:rounded-md duration-300 hover:ml-[-10px] px-6  bg-slate-500 text-white"
+          download={true}
+        >
+          Resume <BsFillPersonFill size={30} />
+        </Link>
+      </ul>
+    </div>
+  );
 };
 
 export default SocialLink;
